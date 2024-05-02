a7fec31de456682883c5dc800cdde64fbee7a29a
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-05-02 17:43:02 -0300

    events-card created

---
+++ packages/design-system/src/atoms/card.tsx
@@ -0,0 +1,7 @@
+import { Card as BaseCard, type CardProps } from 'antd';
+
+export const Card = ({ children, ...props } : CardProps) => {
+    return <BaseCard {...props}>{children}</BaseCard>;
+};
+
+export const CardMeta = BaseCard.Meta;
---
+++ packages/design-system/src/molecules/events-card.tsx
@@ -0,0 +1,29 @@
+import { Button, Card, CardMeta, Image } from '../atoms';
+import { cn } from "../utils";
+
+interface EventCardPros {
+    title: string;
+    description: string;
+    imgSrc: string;
+}
+
+export const EventCard: React.FC<EventCardPros> = ({ title, description, imgSrc }) => {
+    return (
+        <Card 
+            cover={
+                <Image
+                    alt={title}
+                    className={cn('w-[150px] h-[150px] object-cover')}
+                    src={imgSrc}
+                    fallback='https://via.placeholder.com/150'
+                />
+            } 
+        >
+            <CardMeta 
+                title={title}
+                description={description}
+            />
+            <Button>{"Saiba mais >"}</Button>
+        </Card>
+    );
+};
