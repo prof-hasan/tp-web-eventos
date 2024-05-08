2fdf641a7df23c9093c3328302a920cbc2b308bd
Victor Le Roy <victorlrmatos@gmail.com>
2024-05-08 12:49:49 -0300

    removing dark theme 2

--- packages/design-system/src/atoms/button.tsx
+++ packages/design-system/src/atoms/button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = React.ComponentProps<'button'> & {
   variant?: 'primary' | 'secondary';
 };
 
-const baseButtonStyle = ({ variant, variantTheme }: ButtonProps) => {
+const baseButtonStyle = ({ variant }: ButtonProps) => {
   return cn(`rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm`, {
     'bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600':
       variant === 'primary',
