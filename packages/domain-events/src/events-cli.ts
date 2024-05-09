f65e964568c180d7945e8a9702a0cb3686ecea05
Victor Le Roy <victorlrmatos@gmail.com>
2024-05-09 13:09:38 -0300

    disabling eslint file in event cli because of template file of supabase

--- packages/domain-events/src/events-cli.ts
+++ packages/domain-events/src/events-cli.ts
@@ -1,3 +1,5 @@
+/* eslint-disable -- disable eslint for this file because it is a template file and should not be linted */
+
 import { createBrowserClient, createServerClient, type CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import { NextResponse, type NextRequest } from 'next/server';
@@ -8,100 +10,120 @@ export const events = {
     Events(
       (() => {
         const cookieStore = cookies();
-        return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-          cookies: {
-            get(name: string) {
-              return cookieStore.get(name)?.value;
+        return createServerClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL ? process.env.NEXT_PUBLIC_SUPABASE_URL : '',
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY : '',
+          {
+            cookies: {
+              get(name: string) {
+                return cookieStore.get(name)?.value;
+              },
             },
           },
-        });
+        );
       })(),
     ),
   forServerAction: (): any =>
     Events(
       (() => {
         const cookieStore = cookies();
-        return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-          cookies: {
-            get(name: string) {
-              return cookieStore.get(name)?.value;
-            },
-            set(name: string, value: string, options: CookieOptions) {
-              cookieStore.set({ name, value, ...options });
-            },
-            remove(name: string, options: CookieOptions) {
-              cookieStore.set({ name, value: '', ...options });
+        return createServerClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL ? process.env.NEXT_PUBLIC_SUPABASE_URL : '',
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY : '',
+          {
+            cookies: {
+              get(name: string) {
+                return cookieStore.get(name)?.value;
+              },
+              set(name: string, value: string, options: CookieOptions) {
+                cookieStore.set({ name, value, ...options });
+              },
+              remove(name: string, options: CookieOptions) {
+                cookieStore.set({ name, value: '', ...options });
+              },
             },
           },
-        });
+        );
       })(),
     ),
   forClientComponent: (): any =>
     Events(
-      (() => createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!))(),
+      (() =>
+        createBrowserClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL ? process.env.NEXT_PUBLIC_SUPABASE_URL : '',
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY : '',
+        ))(),
     ),
   forRouteHandler: (): any =>
     Events(
       (() => {
         const cookieStore = cookies();
-        return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-          cookies: {
-            get(name: string) {
-              return cookieStore.get(name)?.value;
-            },
-            set(name: string, value: string, options: CookieOptions) {
-              cookieStore.set({ name, value, ...options });
-            },
-            remove(name: string, options: CookieOptions) {
-              cookieStore.set({ name, value: '', ...options });
+        return createServerClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL ? process.env.NEXT_PUBLIC_SUPABASE_URL : '',
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY : '',
+          {
+            cookies: {
+              get(name: string) {
+                return cookieStore.get(name)?.value;
+              },
+              set(name: string, value: string, options: CookieOptions) {
+                cookieStore.set({ name, value, ...options });
+              },
+              remove(name: string, options: CookieOptions) {
+                cookieStore.set({ name, value: '', ...options });
+              },
             },
           },
-        });
+        );
       })(),
     ),
   forMiddleware: ({ request, response }: { request: NextRequest; response: NextResponse }): any =>
     Events(
       (() =>
-        createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-          cookies: {
-            get(name: string) {
-              return request.cookies.get(name)?.value;
-            },
-            set(name: string, value: string, options: CookieOptions) {
-              request.cookies.set({
-                name,
-                value,
-                ...options,
-              });
-              response = NextResponse.next({
-                request: {
-                  headers: request.headers,
-                },
-              });
-              response.cookies.set({
-                name,
-                value,
-                ...options,
-              });
-            },
-            remove(name: string, options: CookieOptions) {
-              request.cookies.set({
-                name,
-                value: '',
-                ...options,
-              });
-              response = NextResponse.next({
-                request: {
-                  headers: request.headers,
-                },
-              });
-              response.cookies.set({
-                name,
-                value: '',
-                ...options,
-              });
+        createServerClient(
+          process.env.NEXT_PUBLIC_SUPABASE_URL ? process.env.NEXT_PUBLIC_SUPABASE_URL : '',
+          process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY : '',
+          {
+            cookies: {
+              get(name: string) {
+                return request.cookies.get(name)?.value;
+              },
+              set(name: string, value: string, options: CookieOptions) {
+                request.cookies.set({
+                  name,
+                  value,
+                  ...options,
+                });
+                response = NextResponse.next({
+                  request: {
+                    headers: request.headers,
+                  },
+                });
+                response.cookies.set({
+                  name,
+                  value,
+                  ...options,
+                });
+              },
+              remove(name: string, options: CookieOptions) {
+                request.cookies.set({
+                  name,
+                  value: '',
+                  ...options,
+                });
+                response = NextResponse.next({
+                  request: {
+                    headers: request.headers,
+                  },
+                });
+                response.cookies.set({
+                  name,
+                  value: '',
+                  ...options,
+                });
+              },
             },
           },
-        }))(),
+        ))(),
     ),
 };
