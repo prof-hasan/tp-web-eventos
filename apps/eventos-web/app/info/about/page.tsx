8c6fdd8c3b4100003c17609ac12ebdcd8da01a37
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-07-10 10:33:53 -0300

    Fix tags in about page

--- apps/eventos-web/app/info/about/page.tsx
+++ apps/eventos-web/app/info/about/page.tsx
@@ -2,37 +2,37 @@ const AboutPage = async () => {
   return (
     <div>
       <div>
-          <h1>Nosso Trabalho</h1>
+          <h3>Nosso Trabalho</h3>
           <div>
             <p>Este site foi produzido por um grupo de alunos para a matéria de <strong>TÓPICOS ESPECIAIS EM ENGENHARIA DE SOFTWARE: PROGRAMAÇÃO PARA WEB</strong> ministrado no CEFET-MG pelo professor Daniel Hasan. O projeto consiste em um sistema de visualização de eventos ficticios (criado por nós) e não ficiticios. Os eventos não ficticios foram obtidos através de APIs de sites como o Sympla e foram utilizados com o intuito de estudar mais técnicas de desenvolvimento WEB.</p>
           </div>
           <div>
             <div>
-                <h2>Front-end</h2>
+                <p><strong>Front-end</strong></p>
                 <p>NextJS + Typescript + Tailwind</p>
             </div>
             <div>
-                <h2>Back-end</h2>
+                <p><strong>Back-end</strong></p>
                 <p>NodeJS</p>
             </div>
           </div>
       </div>
       <div>
-        <h1>Quem Somos</h1>
+        <h3>Quem Somos</h3>
         <div>
             <p>Somos um grupo de estudantes de Engenharia da Computação do CEFET-MG.</p>
             <div>
-                <p>VICTOR LE ROY MATOS</p> 
+                <p><strong>VICTOR LE ROY MATOS</strong></p> 
                 <p>(https://github.com/vmleroy)</p>
                 <p>victorlrmatos@gmail.com</p>
             </div>
             <div>
-                <p>VINICIUS NASCIMENTO SILVA</p>
+                <p><strong>VINICIUS NASCIMENTO SILVA</strong></p>
                 <p>(https://github.com/vnszero)</p>
                 <p>vnszero@gmail.com</p>
             </div>
             <div>
-                <p>WILLIAN SILVA DE ALMEIDA</p>
+                <p><strong>WILLIAN SILVA DE ALMEIDA</strong></p>
                 <p>(https://github.com/willhss1)</p>
                 <p>willian.ws97@yahoo.com</p>
             </div>
