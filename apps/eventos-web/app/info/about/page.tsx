54f22c1a09aae6dd12f2306bb2f9468347119835
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-07-15 15:00:43 -0300

    fix about page tags

--- apps/eventos-web/app/info/about/page.tsx
+++ apps/eventos-web/app/info/about/page.tsx
@@ -1,44 +1,40 @@
 const AboutPage = async () => {
   return (
-    <div>
-      <div>
-          <h3>Nosso Trabalho</h3>
+    <section>
+      <article>
+        <h3>Nosso Trabalho</h3>
+        <p>Este site foi produzido por um grupo de alunos para a matéria de <strong>TÓPICOS ESPECIAIS EM ENGENHARIA DE SOFTWARE: PROGRAMAÇÃO PARA WEB</strong> ministrado no CEFET-MG pelo professor Daniel Hasan. O projeto consiste em um sistema de visualização de eventos ficticios (criado por nós) e não ficiticios. Os eventos não ficticios foram obtidos através de APIs de sites como o Sympla e foram utilizados com o intuito de estudar mais técnicas de desenvolvimento WEB.</p>
+        <span>
           <div>
-            <p>Este site foi produzido por um grupo de alunos para a matéria de <strong>TÓPICOS ESPECIAIS EM ENGENHARIA DE SOFTWARE: PROGRAMAÇÃO PARA WEB</strong> ministrado no CEFET-MG pelo professor Daniel Hasan. O projeto consiste em um sistema de visualização de eventos ficticios (criado por nós) e não ficiticios. Os eventos não ficticios foram obtidos através de APIs de sites como o Sympla e foram utilizados com o intuito de estudar mais técnicas de desenvolvimento WEB.</p>
+            <h4>Front-end</h4>
+            <p>NextJS + Typescript + Tailwind</p>
           </div>
           <div>
-            <div>
-                <p><strong>Front-end</strong></p>
-                <p>NextJS + Typescript + Tailwind</p>
-            </div>
-            <div>
-                <p><strong>Back-end</strong></p>
-                <p>NodeJS</p>
-            </div>
+            <h4>Back-end</h4>
+            <p>NodeJS</p>
           </div>
-      </div>
-      <div>
+        </span>
+      </article>
+      <article>
         <h3>Quem Somos</h3>
+        <p>Somos um grupo de estudantes de Engenharia da Computação do CEFET-MG.</p>
         <div>
-            <p>Somos um grupo de estudantes de Engenharia da Computação do CEFET-MG.</p>
-            <div>
-                <p><strong>VICTOR LE ROY MATOS</strong></p> 
-                <p>(https://github.com/vmleroy)</p>
-                <p>victorlrmatos@gmail.com</p>
-            </div>
-            <div>
-                <p><strong>VINICIUS NASCIMENTO SILVA</strong></p>
-                <p>(https://github.com/vnszero)</p>
-                <p>vnszero@gmail.com</p>
-            </div>
-            <div>
-                <p><strong>WILLIAN SILVA DE ALMEIDA</strong></p>
-                <p>(https://github.com/willhss1)</p>
-                <p>willian.ws97@yahoo.com</p>
-            </div>
+          <h4>VICTOR LE ROY MATOS</h4> 
+          <p>(https://github.com/vmleroy)</p>
+          <p>victorlrmatos@gmail.com</p>
         </div>
-      </div>
-    </div>
+        <div>
+          <h4>VINICIUS NASCIMENTO SILVA</h4>
+          <p>(https://github.com/vnszero)</p>
+          <p>vnszero@gmail.com</p>
+        </div>
+        <div>
+          <h4>WILLIAN SILVA DE ALMEIDA</h4>
+          <p>(https://github.com/willhss1)</p>
+          <p>willian.ws97@yahoo.com</p>
+        </div>
+      </article>
+    </section>
   );
 };
 export default AboutPage;
