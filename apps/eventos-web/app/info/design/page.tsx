c4237af2042752ccc65933c07d420dd0c3ff09bf
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-07-15 15:01:06 -0300

    include heading 4 in design page

--- apps/eventos-web/app/info/design/page.tsx
+++ apps/eventos-web/app/info/design/page.tsx
@@ -1,26 +1,23 @@
 const DesignPage = async () => {
     return (
-      <div>
-        <div>
-            <h3>Tipografia e Paleta</h3>
-            <div>
-              <h2>Heading 2: Roboto, 42px, 2D2327</h2>
-              <h3>heading 3: Roboto,uppercase, 20px #2364AA</h3>
-            </div>
-        </div>
-        <div>
+      <section>
+        <article>
+          <h3>Tipografia e Paleta</h3>
+          <h2>Heading 2: Roboto, 42px, #2D2327</h2>
+          <h3>heading 3: Roboto, uppercase, 20px #2364AA</h3>
+          <h4>Heading 4: Roboto, 18px, Bold, #2D2327</h4>
+        </article>
+        <article>
           <h3>Text: Roboto 20px 2D2327</h3>
-          <div>
-              <p>Pellentesque habitant morbi  senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-              <div>
-                <p><em>TEXTO EM DESTAQUE</em></p>
-                <p><i>TEXTO ESMAECIDO</i></p>
-              </div>
-              <h2>Paleta</h2>
-              <p>imagem</p>
-          </div>
-        </div>
-      </div>
+          <p>Pellentesque habitant morbi  senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          <span>
+            <p><em>TEXTO EM DESTAQUE</em></p>
+            <p><i>TEXTO ESMAECIDO</i></p>
+          </span>
+          <h2>Paleta</h2>
+          <p>imagem</p>
+        </article>
+      </section>
     );
   };
   export default DesignPage;
