9697d6e8d638dbfed2bcf825e29e9e12861a9886
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-07-15 15:02:24 -0300

    Start Terms Page

---
+++ apps/eventos-web/app/info/terms/page.tsx
@@ -0,0 +1,36 @@
+const TermsPage = async () => {
+    return (
+      <section>
+        <article>
+            <p>
+                Entendemos que seu tempo é precioso. Por isso, destacamos abaixo os pontos mais essenciais para o uso da nossa plataforma. No entanto, recomendamos que você leia o documento completo, certo?
+            </p>
+            <p>
+                    A DW Events é uma plataforma que facilita a venda de ingressos, inscrições para eventos, cobrança de contribuições e a gestão de participantes. Todos os eventos publicados são de responsabilidade dos seus organizadores, pois a DW Events não os produz. Os organizadores definem a quantidade e o valor dos ingressos, regras de acesso aos eventos, políticas de reembolso e troca de titularidade, entre outros aspectos.
+            </p>
+            <p>
+                A DW Events não se responsabiliza por ingressos adquiridos fora da sua plataforma.
+            </p>
+            <p>
+                Todas as compras realizadas na plataforma DW Events passam por um processo de análise, seja para conferir os dados preenchidos durante a compra (cartão de crédito) ou para receber a confirmação de pagamento (boleto bancário). Caso haja alguma inconsistência ou o pagamento não seja confirmado, a compra será automaticamente cancelada.
+            </p>
+            <p>
+                A política de troca de titularidade é estabelecida pelo organizador do evento, podendo ou não ser disponibilizada. É responsabilidade do consumidor se informar sobre a política específica do evento de interesse.
+            </p>
+            <p>
+                Caso o participante receba um ingresso por meio de troca de titularidade, é importante saber que o titular da compra pode, a qualquer momento, e desde que autorizado pelo organizador, cancelar a compra ou realizar uma nova troca de titularidade, o que pode impedir o acesso ao evento.
+            </p>
+            <p>
+                A política de reembolso é determinada pelo organizador do evento. É responsabilidade do consumidor se informar sobre a política específica do evento de interesse e, se necessário, entrar em contato com o organizador para solicitar o cancelamento da compra e o reembolso do ingresso.
+            </p>
+            <p>
+                Os ingressos, inscrições ou contribuições adquiridos através da plataforma DW Events serão enviados ao e-mail fornecido assim que o pagamento for aprovado. Também é possível acessá-los no site e no app da DW Events, desde que haja uma conta associada ao mesmo e-mail informado no formulário de compra.
+            </p>
+            <p>
+                Para receber nossas mensagens corretamente, você concorda em verificar as configurações do sistema anti-spam, ou similares, na sua caixa de e-mails. O não recebimento do ingresso no e-mail devido a erros nessas configurações não gerará qualquer expectativa de direito ou indenização.
+            </p>
+        </article>
+      </section>
+    );
+  };
+  export default TermsPage;
