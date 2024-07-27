ac7287d485aaa5b01a3f625b3a8b2981f61d4c08
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-07-27 18:55:56 -0300

    typograph component in About Page

--- apps/eventos-web/app/info/about/page.tsx
+++ apps/eventos-web/app/info/about/page.tsx
@@ -1,39 +1,40 @@
 import { cn } from '../../../../../packages/design-system/src/utils';
+import { Typograph } from '@repo/design-system/atoms';
 
 const AboutPage = async () => {
   return (
     <section className={cn('space-y-10')}>
       <article className={cn('flex flex-col items-center justify-center space-y-5 bg-red-50 py-8 px-20')}>
-        <h3 className={cn('font-bold text-blue-800')}>Nosso Trabalho</h3>
-        <p>Este site foi produzido por um grupo de alunos para a matéria de <strong>TÓPICOS ESPECIAIS EM ENGENHARIA DE SOFTWARE: PROGRAMAÇÃO PARA WEB</strong> ministrado no CEFET-MG pelo professor Daniel Hasan. O projeto consiste em um sistema de visualização de eventos ficticios (criado por nós) e não ficiticios. Os eventos não ficticios foram obtidos através de APIs de sites como o Sympla e foram utilizados com o intuito de estudar mais técnicas de desenvolvimento WEB.</p>
+        <Typograph variant='h3'>Nosso Trabalho</Typograph>
+        <Typograph>Este site foi produzido por um grupo de alunos para a matéria de <strong>TÓPICOS ESPECIAIS EM ENGENHARIA DE SOFTWARE: PROGRAMAÇÃO PARA WEB</strong> ministrado no CEFET-MG pelo professor Daniel Hasan. O projeto consiste em um sistema de visualização de eventos ficticios (criado por nós) e não ficiticios. Os eventos não ficticios foram obtidos através de APIs de sites como o Sympla e foram utilizados com o intuito de estudar mais técnicas de desenvolvimento WEB.</Typograph>
         <span className={cn('flex space-x-80')}>
           <div className={cn('flex flex-col items-center justify-center')}>
-            <h4 className={cn('font-bold')}>Front-end</h4>
-            <p>NextJS + Typescript + Tailwind</p>
+            <Typograph variant='h4'>Front-end</Typograph>
+            <Typograph>NextJS + Typescript + Tailwind</Typograph>
           </div>
           <div className={cn('flex flex-col items-center justify-center')}>
-            <h4 className={cn('font-bold')}>Back-end</h4>
-            <p>NodeJS</p>
+            <Typograph variant='h4'>Back-end</Typograph>
+            <Typograph>NodeJS</Typograph>
           </div>
         </span>
       </article>
       <article className={cn('flex flex-col items-center justify-center space-y-5')}>
-        <h3 className={cn('font-bold text-blue-800')}>Quem Somos</h3>
-        <p>Somos um grupo de estudantes de Engenharia da Computação do CEFET-MG.</p>
+        <Typograph variant='h3'>Quem Somos</Typograph>
+        <Typograph>Somos um grupo de estudantes de Engenharia da Computação do CEFET-MG.</Typograph>
         <div className={cn('flex flex-col items-center justify-center')}>
-          <h4 className={cn('font-bold')}>VICTOR LE ROY MATOS</h4> 
-          <p>(https://github.com/vmleroy)</p>
-          <p>victorlrmatos@gmail.com</p>
+          <Typograph variant='h4'>VICTOR LE ROY MATOS</Typograph> 
+          <Typograph>(https://github.com/vmleroy)</Typograph>
+          <Typograph>victorlrmatos@gmail.com</Typograph>
         </div>
         <div className={cn('flex flex-col items-center justify-center')}>
-          <h4 className={cn('font-bold')}>VINICIUS NASCIMENTO SILVA</h4>
-          <p>(https://github.com/vnszero)</p>
-          <p>vnszero@gmail.com</p>
+          <Typograph variant='h4'>VINICIUS NASCIMENTO SILVA</Typograph>
+          <Typograph>(https://github.com/vnszero)</Typograph>
+          <Typograph>vnszero@gmail.com</Typograph>
         </div>
         <div className={cn('flex flex-col items-center justify-center')}>
-          <h4 className={cn('font-bold')}>WILLIAN SILVA DE ALMEIDA</h4>
-          <p>(https://github.com/willhss1)</p>
-          <p>willian.ws97@yahoo.com</p>
+          <Typograph variant='h4'>WILLIAN SILVA DE ALMEIDA</Typograph>
+          <Typograph>(https://github.com/willhss1)</Typograph>
+          <Typograph>willian.ws97@yahoo.com</Typograph>
         </div>
       </article>
     </section>
