f2db474f3921258159f6ad0c0fd73d0814cb4255
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-07-27 19:59:44 -0300

    add typograph to terms page

--- apps/eventos-web/app/info/terms/page.tsx
+++ apps/eventos-web/app/info/terms/page.tsx
@@ -1,34 +1,37 @@
+import { cn } from '../../../../../packages/design-system/src/utils';
+import { Typograph } from '@repo/design-system/atoms';
+
 const TermsPage = async () => {
     return (
-      <section>
-        <article>
-            <p>
+      <section className={cn('space-y-10')}>
+        <article className={cn('flex flex-col items-center justify-center space-y-5 bg-red-50 py-8 px-20')}>
+            <Typograph>
                 Entendemos que seu tempo é precioso. Por isso, destacamos abaixo os pontos mais essenciais para o uso da nossa plataforma. No entanto, recomendamos que você leia o documento completo, certo?
-            </p>
-            <p>
-                    A DW Events é uma plataforma que facilita a venda de ingressos, inscrições para eventos, cobrança de contribuições e a gestão de participantes. Todos os eventos publicados são de responsabilidade dos seus organizadores, pois a DW Events não os produz. Os organizadores definem a quantidade e o valor dos ingressos, regras de acesso aos eventos, políticas de reembolso e troca de titularidade, entre outros aspectos.
-            </p>
-            <p>
+            </Typograph>
+            <Typograph>
+                A DW Events é uma plataforma que facilita a venda de ingressos, inscrições para eventos, cobrança de contribuições e a gestão de participantes. Todos os eventos publicados são de responsabilidade dos seus organizadores, pois a DW Events não os produz. Os organizadores definem a quantidade e o valor dos ingressos, regras de acesso aos eventos, políticas de reembolso e troca de titularidade, entre outros aspectos.
+            </Typograph>
+            <Typograph>
                 A DW Events não se responsabiliza por ingressos adquiridos fora da sua plataforma.
-            </p>
-            <p>
+            </Typograph>
+            <Typograph>
                 Todas as compras realizadas na plataforma DW Events passam por um processo de análise, seja para conferir os dados preenchidos durante a compra (cartão de crédito) ou para receber a confirmação de pagamento (boleto bancário). Caso haja alguma inconsistência ou o pagamento não seja confirmado, a compra será automaticamente cancelada.
-            </p>
-            <p>
+            </Typograph>
+            <Typograph>
                 A política de troca de titularidade é estabelecida pelo organizador do evento, podendo ou não ser disponibilizada. É responsabilidade do consumidor se informar sobre a política específica do evento de interesse.
-            </p>
-            <p>
+            </Typograph>
+            <Typograph>
                 Caso o participante receba um ingresso por meio de troca de titularidade, é importante saber que o titular da compra pode, a qualquer momento, e desde que autorizado pelo organizador, cancelar a compra ou realizar uma nova troca de titularidade, o que pode impedir o acesso ao evento.
-            </p>
-            <p>
+            </Typograph>
+            <Typograph>
                 A política de reembolso é determinada pelo organizador do evento. É responsabilidade do consumidor se informar sobre a política específica do evento de interesse e, se necessário, entrar em contato com o organizador para solicitar o cancelamento da compra e o reembolso do ingresso.
-            </p>
-            <p>
+            </Typograph>
+            <Typograph>
                 Os ingressos, inscrições ou contribuições adquiridos através da plataforma DW Events serão enviados ao e-mail fornecido assim que o pagamento for aprovado. Também é possível acessá-los no site e no app da DW Events, desde que haja uma conta associada ao mesmo e-mail informado no formulário de compra.
-            </p>
-            <p>
+            </Typograph>
+            <Typograph>
                 Para receber nossas mensagens corretamente, você concorda em verificar as configurações do sistema anti-spam, ou similares, na sua caixa de e-mails. O não recebimento do ingresso no e-mail devido a erros nessas configurações não gerará qualquer expectativa de direito ou indenização.
-            </p>
+            </Typograph>
         </article>
       </section>
     );
